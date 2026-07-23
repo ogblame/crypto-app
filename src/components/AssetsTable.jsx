21c710442cd78f7ac694bd1c6e9a1042113b3334
devlyst <dimakuzmintcev@gmail.com>
2026-07-23 11:22:58 +0300

    add dynamic table in assetsTable component

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,5 +1,48 @@
 import React from "react";
 
+import { Table } from "antd";
+import { useCrypto } from "../context/crypto-context";
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    showSorterTooltip: { target: "full-header" },
+    // specify the condition of filtering result
+    // here is that finding the name started with `value`
+    onFilter: (value, record) => record.name.indexOf(value) === 0,
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ["descend"],
+  },
+  {
+    title: "Price, $",
+    dataIndex: "price",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: "Amount",
+    dataIndex: "amount",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
+
 export default function AssetsTable() {
-  return <div>AssetsTable</div>;
+  const { assets } = useCrypto();
+
+  const data = assets.map((asset) => ({
+    key: asset.id,
+    name: asset.name,
+    price: asset.price,
+    amount: asset.amount,
+  }));
+
+  return (
+    <Table
+      pagination={false}
+      columns={columns}
+      dataSource={data}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
+  );
 }
